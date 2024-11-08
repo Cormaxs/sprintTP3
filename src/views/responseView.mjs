6c61ea8916b7ec87807ab789940e1d414fd17966
cormaxs <tomasbarros076@gmail.com>
2024-11-08 22:15:23 +0100

    todo

---
+++ src/views/responseView.mjs
@@ -0,0 +1,18 @@
+//MODULO PARA FORMATEAR LA RESPUESTA DE SUPERHEROES
+export function renderizarSuperheroe(superheroe) {
+    return {
+        Nombre: superheroe.nombreSuperHeroe,
+        "Nombre Real": superheroe.nombreReal,
+        Edad: superheroe.edad,
+        "Planeta de Origen": superheroe.planetaOrigen,
+        Debilidad: superheroe.debilidad,
+        Poderes: superheroe.poderes,
+        Aliados: superheroe.aliados,
+        Enemigos: superheroe.enemigos,
+        "Cargado Por": superheroe.nombrePersonaCargo  //Agregar el nombre de la persona que lo cargo
+    };
+}
+
+export function renderizarListaSuperheroes(superheroes) {
+    return superheroes.map(superheroe => renderizarSuperheroe(superheroe));
+}
